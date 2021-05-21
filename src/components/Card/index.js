418db7eb90d7ca5e400fe6c9697f7e1350142ba9
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-05-21 17:16:19 -0300

    feat: remove card component and his style

--- src/components/Card/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-import { styles } from "./styles";
-
-const Card = ({ children }) => {
-  return <View style={styles.Card}>{children}</View>;
-};
-export default Card;
