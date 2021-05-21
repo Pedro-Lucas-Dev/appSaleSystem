dc0ac4bdf69d15cd6d092696cc68aa98c99951ee
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-05-21 15:11:02 -0300

    feat: create Login screen

---
+++ src/modules/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+import Layout from "../../../components/Layout";
+
+const Login = () => {
+  return (
+    <Layout>
+      <Text>Welcome</Text>
+    </Layout>
+  );
+};
+export default Login;
