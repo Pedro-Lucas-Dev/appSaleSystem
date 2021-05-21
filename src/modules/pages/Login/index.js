840c0acf302cda0f3e653506e087826340b2605a
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-05-21 16:03:01 -0300

    feat: add card component to login screen

--- src/modules/pages/Login/index.js
+++ src/modules/pages/Login/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Text } from "react-native";
+import Card from "../../../components/Card";
 import Layout from "../../../components/Layout";
 
 const Login = () => {
   return (
     <Layout>
-      <Text>Welcome</Text>
+      <Card>
+        <Text>Welcome</Text>
+      </Card>
     </Layout>
   );
 };
