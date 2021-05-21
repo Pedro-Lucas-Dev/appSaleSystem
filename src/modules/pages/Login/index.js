0c5719c9363a49a80c52a07cbd3e8aa81030c189
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-05-21 18:44:19 -0300

    feat: add controlled input to login screen

--- src/modules/pages/Login/index.js
+++ src/modules/pages/Login/index.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text } from "react-native";
+import Input from "../../../components/Input";
 import Layout from "../../../components/Layout";
 
 const Login = () => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const updateLoginData = (id, value) => {
+    setLoginData({ ...loginData, [id]: value });
+  };
+
   return (
     <Layout>
       <Text>Welcome</Text>
+      <Input
+        placeholder="email"
+        value={loginData.email}
+        onChangeText={(inputName) => updateLoginData("email", inputName)}
+      />
+      <Input
+        placeholder="senha"
+        value={loginData.password}
+        onChangeText={(inputName) => updateLoginData("password", inputName)}
+        secureTextEntry
+      />
     </Layout>
   );
 };
