ac753f81aa24cc55b3e81d320e5c4e92a451b8dc
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-05-21 15:02:17 -0300

    feat: remove unnecessary things

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import Layout from "./src/components/Layout";
 
 export default function App() {
@@ -12,10 +12,10 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
-});
+};
