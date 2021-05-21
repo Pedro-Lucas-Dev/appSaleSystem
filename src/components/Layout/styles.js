411a0047c3b61584a8ebb107da216a841eddecab
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-05-21 15:01:37 -0300

    fix: change color in layout component

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -3,7 +3,7 @@ import { Platform, StatusBar } from "react-native";
 export const styles = {
   LayoutContainer: {
     flex: 1,
-    backgroundColor: "#FF5733",
+    backgroundColor: "#05D4FE",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 70,
   },
 };
